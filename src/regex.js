eb50c324635dbed253d60c4771406933b8376ee3
Tim Everington <tim.everington@rivieratravel.co.uk>
2019-11-28 11:34:02 +0000

    fix for uppercase arg names in route definitions

--- src/regex.js
+++ src/regex.js
@@ -2,7 +2,7 @@
 export const arg = {
 	isArray: /\[arr\((int|num|str|bool)\)\]/,
 	type: /\[(int|num|str|bool)\]/,
-	name: /::(?:\?)?([a-z0-9_-]+)/,
+	name: /::(?:\?)?([a-zA-Z0-9_-]+)/,
 }
 
 // extract arguments of type from URL
